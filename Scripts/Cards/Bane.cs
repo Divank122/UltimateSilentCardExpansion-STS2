5f96d50fd6fbad8b652718b1c06ae9c8c310a664
Divank <Divank122@gmail.com>
2026-08-08 04:28:32 +0800

    1. 重做厄咒，现在特别有设计；2. 移除扎透、护符和游刃有余；

--- Scripts/Cards/Bane.cs
+++ Scripts/Cards/Bane.cs
@@ -17,31 +17,21 @@ namespace USCE.Scripts.Cards;
 [Pool(typeof(SilentCardPool))]
 public class Bane : SilentCardModel, ILocalizationProvider
 {
-    private const int energyCost = 1;
-    private const CardType type = CardType.Skill;
+    private const int energyCost = 3;
+    private const CardType type = CardType.Power;
     private const CardRarity rarity = CardRarity.Rare;
     private const TargetType targetType = TargetType.Self;
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new DynamicVar("IntangiblePower", 1m),
-        new DynamicVar("PoisonPower", 9m),
-        new DynamicVar("PoisonLoss", 2m)
+        new PowerVar<IntangiblePower>("IntangibleAmount", 1m),
+        new PowerVar<BanePower>("BanePower", 1m)
     ];
-
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain, CardKeyword.Exhaust];
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
-        HoverTipFactory.FromPower<IntangiblePower>(),
-        HoverTipFactory.FromPower<PoisonPower>()
+        HoverTipFactory.FromPower<IntangiblePower>()
     ];
-
-    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
-    {
-        "zhs" => new CardLoc("厄咒", "获得{IntangiblePower:diff()}层[gold]无实体[/gold]和{PoisonPower:diff()}层[gold]中毒[/gold]。\n每当本回合你受到攻击时，失去{PoisonLoss:diff()}层[gold]中毒[/gold]。"),
-        _ => new CardLoc("Bane", "Gain {IntangiblePower:diff()} [gold]Intangible[/gold] and {PoisonPower:diff()} [gold]Poison[/gold].\nWhenever you are attacked this turn, lose {PoisonLoss:diff()} [gold]Poison[/gold].")
-    };
 
     public Bane() : base(energyCost, type, rarity, targetType)
     {
@@ -49,18 +39,19 @@ public class Bane : SilentCardModel, ILocalizationProvider
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        int intangibleAmount = DynamicVars["IntangiblePower"].IntValue;
-        int poisonAmount = DynamicVars["PoisonPower"].IntValue;
-        int poisonLoss = DynamicVars["PoisonLoss"].IntValue;
-
-        await PowerCmd.Apply<IntangiblePower>(choiceContext, Owner.Creature, intangibleAmount, Owner.Creature, this);
-        await PowerCmd.Apply<PoisonPower>(choiceContext, Owner.Creature, poisonAmount, Owner.Creature, this);
-        await PowerCmd.Apply<BanePower>(choiceContext, Owner.Creature, poisonLoss, Owner.Creature, this);
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<IntangiblePower>(choiceContext, Owner.Creature, DynamicVars["IntangibleAmount"].BaseValue, Owner.Creature, this);
+        await PowerCmd.Apply<BanePower>(choiceContext, Owner.Creature, DynamicVars["BanePower"].BaseValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        base.EnergyCost.UpgradeBy(-1);
-        DynamicVars["PoisonPower"].UpgradeValueBy(-2m);
+        EnergyCost.UpgradeBy(-1);
     }
+
+    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
+    {
+        "zhs" => new CardLoc("厄咒", "获得[blue]{IntangibleAmount:diff()}[/blue]层[gold]无实体[/gold]。\n[gold]手牌[/gold]上限减少[blue]{BanePower}[/blue]。\n每回合开始时，将[gold]手牌[/gold]中的一张牌变化为{IfUpgraded:show:[gold]厄咒+[/gold]|[gold]厄咒[/gold]}。"),
+        _ => new CardLoc("Bane", "Gain [blue]{IntangibleAmount:diff()}[/blue] [gold]Intangible[/gold].\nReduce your max [gold]Hand[/gold] size by [blue]{BanePower}[/blue].\nAt the start of each turn, transform a card in your [gold]hand[/gold] into a {IfUpgraded:show:[gold]Bane+[/gold]|[gold]Bane[/gold]}.")
+    };
 }
--- Scripts/Powers/BanePower.cs
+++ Scripts/Powers/BanePower.cs
@@ -1,60 +1,99 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using BaseLib.Abstracts;
-using BaseLib.Extensions;
-using MegaCrit.Sts2.Core.Combat;
+using BaseLib.Hooks;
+using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Powers;
-using MegaCrit.Sts2.Core.ValueProps;
+using USCE.Scripts.Cards;
 
 namespace USCE.Scripts.Powers;
 
-public class BanePower : CustomPowerModel
+public class BanePower : CustomPowerModel, IMaxHandSizeModifier
 {
+    private class Data
+    {
+        public int UpgradedCount;
+    }
+
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
 
     public override string? CustomPackedIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_bane_power.png";
     public override string? CustomBigIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_bane_power.png";
 
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DynamicVar("BaseCount", 0m),
+        new DynamicVar("UpgradedCount", 0m)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.Static(StaticHoverTip.Transform)
+    ];
+
+    protected override object? InitInternalData()
+    {
+        return new Data();
+    }
+
+    public override Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        if (power == this && cardSource is Bane bane)
+        {
+            Data data = GetInternalData<Data>();
+            if (bane.IsUpgraded)
+            {
+                data.UpgradedCount++;
+            }
+            DynamicVars["UpgradedCount"].BaseValue = data.UpgradedCount;
+            DynamicVars["BaseCount"].BaseValue = Amount - data.UpgradedCount;
+        }
+        return Task.CompletedTask;
+    }
+
+    public int ModifyMaxHandSize(Player player, int currentMaxHandSize)
+    {
+        return currentMaxHandSize - (int)Amount;
+    }
+
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != Owner.Player)
+        {
+            return;
+        }
+
+        int upgradedCount = GetInternalData<Data>().UpgradedCount;
+        CardSelectorPrefs prefs = new CardSelectorPrefs(CardSelectorPrefs.TransformSelectionPrompt, (int)Amount);
+        List<CardModel> selected = (await CardSelectCmd.FromHand(choiceContext, player, prefs, null, this)).ToList();
+
+        int upgradesToMake = Math.Min(selected.Count, upgradedCount);
+        for (int i = 0; i < selected.Count; i++)
+        {
+            CardModel card = selected[i];
+            CardModel replacement = card.CardScope.CreateCard<Bane>(card.Owner);
+            if (i < upgradesToMake)
+            {
+                CardCmd.Upgrade(replacement);
+            }
+            await CardCmd.Transform(card, replacement);
+        }
+    }
+
     public override List<(string, string)>? Localization => LocManager.Instance.Language switch
     {
-        "zhs" => new PowerLoc("厄咒", "每当本回合你受到攻击时，失去2层中毒。", "每当本回合你受到攻击时，失去[blue]{Amount}[/blue]层[gold]中毒[/gold]。"),
-        _ => new PowerLoc("Bane", "Lose 2 Poison each time you are attacked this turn.", "Lose [blue]{Amount}[/blue] [gold]Poison[/gold] each time you are attacked this turn.")
+        "zhs" => new PowerLoc("厄咒", "[gold]手牌[/gold]上限减少[blue]{Amount}[/blue]。\n每回合开始时，将[gold]手牌[/gold]中的{UpgradedCount:cond:>0?[blue]{UpgradedCount}[/blue]张牌变化为[gold]厄咒+[/gold]{BaseCount:cond:>0?，[blue]{BaseCount}[/blue]张牌变化为[gold]厄咒[/gold]|}|[blue]{BaseCount}[/blue]张牌变化为[gold]厄咒[/gold]}。", "[gold]手牌[/gold]上限减少[blue]{Amount}[/blue]。\n每回合开始时，将[gold]手牌[/gold]中的{UpgradedCount:cond:>0?[blue]{UpgradedCount}[/blue]张牌变化为[gold]厄咒+[/gold]{BaseCount:cond:>0?，[blue]{BaseCount}[/blue]张牌变化为[gold]厄咒[/gold]|}|[blue]{BaseCount}[/blue]张牌变化为[gold]厄咒[/gold]}。"),
+        _ => new PowerLoc("Bane", "Reduce your max [gold]Hand[/gold] size by [blue]{Amount}[/blue].\nAt the start of each turn, transform {UpgradedCount:cond:>0?[blue]{UpgradedCount}[/blue] cards in your [gold]hand[/gold] into a [gold]Bane+[/gold]{BaseCount:cond:>0?, and [blue]{BaseCount}[/blue] cards into a [gold]Bane[/gold]|}|[blue]{BaseCount}[/blue] cards in your [gold]hand[/gold] into a [gold]Bane[/gold]}.", "Reduce your max [gold]Hand[/gold] size by [blue]{Amount}[/blue].\nAt the start of each turn, transform {UpgradedCount:cond:>0?[blue]{UpgradedCount}[/blue] cards in your [gold]hand[/gold] into a [gold]Bane+[/gold]{BaseCount:cond:>0?, and [blue]{BaseCount}[/blue] cards into a [gold]Bane[/gold]|}|[blue]{BaseCount}[/blue] cards in your [gold]hand[/gold] into a [gold]Bane[/gold]}.")
     };
-
-    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props, Creature? dealer, CardModel? cardSource)
-    {
-        if (target == Owner && props.IsPoweredAttack_())
-        {
-            var poisonPower = Owner!.GetPower<PoisonPower>();
-            
-            if (poisonPower != null && poisonPower.Amount > 0)
-            {
-                int poisonAmount = poisonPower.Amount;
-                int newAmount = poisonAmount - Amount;
-                if (newAmount <= 0)
-                {
-                    await PowerCmd.Remove(poisonPower);
-                }
-                else
-                {
-                    poisonPower.SetAmount(newAmount);
-                }
-            }
-        }
-    }
-
-    public override async Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
-    {
-        if (Owner != null && side != Owner.Side)
-        {
-            await PowerCmd.Remove(this);
-        }
-    }
 }
