bed191cdd134df7f3766d7e5266a37c3a2ae5d36
Divank <Divank122@gmail.com>
2026-08-13 10:40:53 +0800

    重做弹性纤维，现在是获得一定的格挡可以获得覆甲，但是还没决定敲后效果

--- Scripts/Cards/ElasticFiber.cs
+++ Scripts/Cards/ElasticFiber.cs
@@ -4,13 +4,13 @@ using BaseLib.Abstracts;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
 using MegaCrit.Sts2.Core.Models.Powers;
+using USCE.Scripts.Powers;
 
 namespace USCE.Scripts.Cards;
 
@@ -19,15 +19,14 @@ public class ElasticFiber : SilentCardModel, ILocalizationProvider
 {
     private const int energyCost = 1;
 
+    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
+    {
+        new PowerVar<ElasticFiberPower>("Plating", 1m)
+    };
+
     protected override IEnumerable<IHoverTip> ExtraHoverTips => new IHoverTip[]
     {
         HoverTipFactory.FromPower<PlatingPower>()
-    };
-
-    protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
-    {
-        new PowerVar<PlatingPower>("PlayerPlating", 6m),
-        new PowerVar<PlatingPower>("EnemyPlating", 6m)
     };
 
     public ElasticFiber()
@@ -37,27 +36,18 @@ public class ElasticFiber : SilentCardModel, ILocalizationProvider
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<PlatingPower>(choiceContext, Owner.Creature, DynamicVars["PlayerPlating"].IntValue, Owner.Creature, this);
-
-        var enemies = CombatState!.HittableEnemies;
-        if (enemies.Count > 0)
-        {
-            Creature? randomEnemy = Owner.RunState.Rng.CombatTargets.NextItem(enemies);
-            if (randomEnemy != null)
-            {
-                await PowerCmd.Apply<PlatingPower>(choiceContext, randomEnemy, DynamicVars["EnemyPlating"].IntValue, Owner.Creature, this);
-            }
-        }
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<ElasticFiberPower>(choiceContext, Owner.Creature, DynamicVars["Plating"].BaseValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["PlayerPlating"].UpgradeValueBy(2m);
+        DynamicVars["Plating"].UpgradeValueBy(1m);
     }
 
     public override List<(string, string)>? Localization => LocManager.Instance.Language switch
     {
-        "zhs" => new CardLoc("弹性纤维", "获得{PlayerPlating:diff()}层[gold]覆甲[/gold]。\n随机敌人获得{EnemyPlating:diff()}层[gold]覆甲[/gold]。"),
-        _ => new CardLoc("Elastic Fiber", "Gain {PlayerPlating:diff()} [gold]Plating[/gold].\nA random enemy gains {EnemyPlating:diff()} [gold]Plating[/gold].")
+        "zhs" => new CardLoc("弹性纤维", "每获得6点[gold]格挡[/gold]，获得{Plating:diff()}层[gold]覆甲[/gold]。"),
+        _ => new CardLoc("Elastic Fiber", "Whenever you gain 6 [gold]Block[/gold], gain {Plating:diff()} [gold]Plating[/gold].")
     };
 }
---
+++ Scripts/Powers/ElasticFiberPower.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace USCE.Scripts.Powers;
+
+public class ElasticFiberPower : CustomPowerModel
+{
+    private class Data
+    {
+        public decimal BlockGained;
+        public int TriggerCount;
+    }
+
+    private const decimal BlockThreshold = 6m;
+
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override PowerInstanceType InstanceType => PowerInstanceType.Instanced;
+
+    public override int DisplayAmount => (int)BlockThreshold - (int)GetInternalData<Data>().BlockGained % (int)BlockThreshold;
+
+    public override string? CustomPackedIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_elastic_fiber_power.png";
+    public override string? CustomBigIconPath => "res://UltimateSilentCardExpansion/images/powers/usce_elastic_fiber_power.png";
+
+    protected override object? InitInternalData()
+    {
+        return new Data();
+    }
+
+    public override async Task AfterBlockGained(Creature creature, decimal amount, ValueProp props, CardModel? cardSource)
+    {
+        if (creature != Owner || amount <= 0m)
+        {
+            return;
+        }
+
+        Data data = GetInternalData<Data>();
+        data.BlockGained += amount;
+        int triggers = (int)(data.BlockGained / BlockThreshold) - data.TriggerCount;
+        if (triggers > 0)
+        {
+            Flash();
+            await PowerCmd.Apply<PlatingPower>(new ThrowingPlayerChoiceContext(), Owner, Amount * triggers, Owner, null);
+            data.TriggerCount += triggers;
+        }
+        InvokeDisplayAmountChanged();
+    }
+
+    public override List<(string, string)>? Localization => LocManager.Instance.Language switch
+    {
+        "zhs" => new PowerLoc("弹性纤维", "每获得6点[gold]格挡[/gold]，获得[blue]{Amount}[/blue]层[gold]覆甲[/gold]。", "每获得6点[gold]格挡[/gold]，获得[blue]{Amount}[/blue]层[gold]覆甲[/gold]。"),
+        _ => new PowerLoc("Elastic Fiber", "Whenever you gain 6 [gold]Block[/gold], gain [blue]{Amount}[/blue] [gold]Plating[/gold].", "Whenever you gain 6 [gold]Block[/gold], gain [blue]{Amount}[/blue] [gold]Plating[/gold].")
+    };
+}
